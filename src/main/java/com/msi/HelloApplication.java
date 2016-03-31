0c787d57c57000840b81d0ecea20b6a9c4e3d8a6
Joey <Joey>
2016-03-31 11:38:42 -0400

    Initial commit

---
+++ src/main/java/com/msi/HelloApplication.java
@@ -0,0 +1,18 @@
+package com.msi;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by Administrator on 2016/3/31.
+ */
+@SpringBootApplication
+
+public class HelloApplication {
+    public static void  main(String[] args){
+        ApplicationContext ctx = SpringApplication
+                .run(HelloApplication.class,args);
+    }
+}
